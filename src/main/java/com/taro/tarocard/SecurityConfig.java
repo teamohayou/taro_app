9c848f284a028a77cb8aad845da0df6624550c0a
ray-4869 <rlfgustn777@gmail.com>
2024-10-22 12:04:19 +0900

    결과창

--- src/main/java/com/taro/tarocard/SecurityConfig.java
+++ src/main/java/com/taro/tarocard/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -50,3 +52,4 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 }
+
--- src/main/java/com/taro/tarocard/card/CardController.java
+++ src/main/java/com/taro/tarocard/card/CardController.java
@@ -39,9 +39,9 @@ public class CardController {
     public String getCardResult(@PathVariable("rcid") Integer rcid, Model model) {
         Optional<RomanticCard> rcCard = romanticCardRepository.findById(rcid);
         if (rcCard.isPresent()) {
-            String imagePath = "/images/" + rcid + ".webp"; // ID를 기반으로 이미지 파일 경로 생성
-            model.addAttribute("rcCard", rcCard.get());
-            model.addAttribute("imagePath", imagePath); // 이미지 경로를 모델에 추가
+            RomanticCard card = rcCard.get();
+            model.addAttribute("rcCard", card);
+            model.addAttribute("imagePath", card.getImage_path());
             return "cardresult_page"; // 정상적인 결과 페이지
         }else {
             throw new RuntimeException("카드를 찾을 수 없습니다.");
